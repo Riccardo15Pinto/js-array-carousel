a381200d538975682536e6e40edd88a70f4d9d2d
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-01 15:06:29 +0200

    take element from dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 //check console log
 console.log('ok js');
-//
+//richiamo elementi dal DOM
+const firstgalleryelement = document.getElementById('main-gallery');
+const secondgalleryelement = document.getElementById('second-gallery');
+const beforebuttonelement = document.getElementById('left-side');
+const afterbuttonelement = document.getElementById('right-side');
