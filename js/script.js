040439305c6fc7e3e5115154e95068758e06ef05
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-01 15:08:50 +0200

    create array

--- js/script.js
+++ js/script.js
@@ -5,3 +5,5 @@ const firstgalleryelement = document.getElementById('main-gallery');
 const secondgalleryelement = document.getElementById('second-gallery');
 const beforebuttonelement = document.getElementById('left-side');
 const afterbuttonelement = document.getElementById('right-side');
+// creo un ARRAY
+const images = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp',];
