b58e2aecf988fb47b48cda281bca973f549fa81e
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-01 20:41:14 +0200

    set opacity in imageelementsmin

--- js/script.js
+++ js/script.js
@@ -9,23 +9,31 @@ const afterbuttonelement = document.getElementById('right-side');
 const images = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp',];
 //preparo le immagini
 let imagecontent = '';
+let imagecontentmin = '';
 //per ogni elemento dell array creo un tag img
 for(let i = 0; i < images.length; i++){
     imagecontent += `<img src="${images[i]}" class="none">`;
+    imagecontentmin += `<div class="opacity"><img src="${images[i]}"></div>`;
 }
 //inserisco le immagini nel DOM
 firstgalleryelement.innerHTML = imagecontent ;
+secondgalleryelement.innerHTML = imagecontentmin ;
+
 //recupero tutte le immagini
 const imageElements = document.querySelectorAll('#main-gallery img');
+const imageElementsmin = document.querySelectorAll('#second-gallery div');
 //preparo la gestione dell'index
 let currentIndex = 0;
 //impostiamo la prima immagine come quella attiva
 imageElements[currentIndex].classList.add('active');
+imageElementsmin[currentIndex].classList.remove('opacity');
 
 afterbuttonelement.addEventListener('click', function(){
-
-    
     imageElements[currentIndex].classList.remove('active');
+    imageElementsmin[currentIndex].classList.add('opacity');
+    
+    
+    
     
     if(currentIndex === images.length - 1){
         currentIndex = 0;
@@ -34,8 +42,10 @@ afterbuttonelement.addEventListener('click', function(){
         currentIndex++
         
     }
-        
+    
+    imageElementsmin[currentIndex].classList.remove('opacity');
     imageElements[currentIndex].classList.add('active');
+
 
     console.log(currentIndex)
 
@@ -44,6 +54,7 @@ afterbuttonelement.addEventListener('click', function(){
 beforebuttonelement.addEventListener('click', function(){
 
     imageElements[currentIndex].classList.remove('active');
+    imageElementsmin[currentIndex].classList.add('opacity');
     
     if(currentIndex < 1 ){
 
@@ -53,6 +64,8 @@ beforebuttonelement.addEventListener('click', function(){
 
         currentIndex--
     }
+
+    imageElementsmin[currentIndex].classList.remove('opacity');
         
     imageElements[currentIndex].classList.add('active');
 
@@ -63,7 +76,5 @@ beforebuttonelement.addEventListener('click', function(){
 
 
 console.log(imagecontent)
-console.table(images)
-   
-    
+console.table(images)  
 console.log(firstgalleryelement)
